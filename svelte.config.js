d164fa7cbc8ee6460084860613051893cca72c9d
thebjorn <bp@datakortet.no>
2024-01-15 12:45:58 +0100

    shadcn init

--- svelte.config.js
+++ svelte.config.js
@@ -5,13 +5,25 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			// These are the default values, no need to set them unless you want to change them.
+			// $assets: '/assets',
+			// $components: '/components',
+			// $layouts: '/layouts',
+			// $lib: '/lib',
+			// $routes: '/routes',
+			// $static: '/static',
+			// $stores: '/stores',
+			// $utils: '/utils'
+			"@/*": "./src/lib/*",
+		},
 	}
 };
 
